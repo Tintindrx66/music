4dba13b01d5c07a75c3e0732b0e24d94afb81e51
Tintindrx66 <drx89820704@gmail.com>
2020-02-07 18:23:34 -0500

    modified

--- src/components/home/ArtistsList.js
+++ src/components/home/ArtistsList.js
@@ -6,22 +6,20 @@ const ArtistsList = ({ allArtists }) => {
     <div className="artists_list">
       <h4>Artists</h4>
       <div className="artist_container">
-        <div style={{ textAlign: "center" }}>
-          {allArtists
-            ? allArtists.map(artist => (
-                <Link
-                  key={artist.id}
-                  to={`/artist/${artist.id}`}
-                  className="artist_item"
-                  style={{
-                    background: `url("/images/covers/${artist.cover}.jpg") no-repeat`
-                  }}
-                >
-                  <div>{artist.name}</div>
-                </Link>
-              ))
-            : null}
-        </div>
+        {allArtists
+          ? allArtists.map(artist => (
+              <Link
+                key={artist.id}
+                to={`/artist/${artist.id}`}
+                className="artist_item"
+                style={{
+                  background: `url("/images/covers/${artist.cover}.jpg") no-repeat`
+                }}
+              >
+                <div>{artist.name}</div>
+              </Link>
+            ))
+          : null}
       </div>
     </div>
   );
